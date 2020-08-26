beedfa3221ea6405da1070492614052b86036d50
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-08-26 14:35:12 -0700

    added shipping details

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+	path('update_item/', views.checkout, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
 
-	
 
 ]
